c6047bba11e6648c53e181ca52232ec7a95f2415
ZG.P <zhigang.pu@whu.edu.cn>
2018-10-26 20:37:32 +0800

    20181026

--- tutorial/tutorial/spiders/quotes_spiders.py
+++ tutorial/tutorial/spiders/quotes_spiders.py
@@ -1,19 +1,24 @@
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    
-    def start_request(self):
+    name = "MLmastery"
+
+    def start_requests(self):
         urls = [
-                'http://quotes.toscrape.com/page/1/',
-                'http://quotes.toscrape.com/page/2/',
+                'https://machinelearningmastery.com/blog/',
                 ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_link)
         
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+    def parse_link(self, response):
+        article_urls = response.xpath('//section[@id="main"]//article/a/@href').extract()
+        for url in article_urls:
+            yield scrapy.Request(url, callback=self.parse_article)
+            
+    def parse_article(self, response):
+        page = response.url.split('/')[-2]
+        filename = '%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log('Saved file %s' % filename)
+        self.logger.info('Saved file %s' % filename)
+        
